49e1db62356248e3429a63c1a91ce52f554e3942
Katie <kagnello@ucvts.tec.nj.us>
2018-01-03 15:51:00 +0000

    Created a Characters Class

---
+++ finalproject.py
@@ -0,0 +1,11 @@
+class Characters(object):
+    def __init__(self, health, inventory, strengths, weaknesses):
+        self.health = 100
+        self.inventory = inventory
+        self.strengths = strengths
+        self.weaknesses = weaknesses
+Caitlin = Characters("Mirror", "Observant", "Reckless")
+Tyler = Characters("Baseball", "Leader", "Stubborn")
+Erin = Characters ("Notebook", "Intelligent", "Perfectionist")
+Matt = Characters("Granola Bar", "Strong", "Uncooperative")
+
